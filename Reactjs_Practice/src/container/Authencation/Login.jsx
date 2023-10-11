f574703794da22f87189c38b2e1fddd2e654000c
HoangMinhFpt <65846831+HoangMinhFpt@users.noreply.github.com>
2023-10-12 00:22:41 +0700

    create login

--- Reactjs_Practice/src/container/Authencation/Login.jsx
+++ Reactjs_Practice/src/container/Authencation/Login.jsx
@@ -2,16 +2,25 @@ import "./Login.scss"
 
 function Login() {
   return (
-    <div className="login-container">
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
-      Login<br/>
+    <div className="login-background-container">
+      <div className="login-container">
+        <div className="title-login">Login</div>
+        <div className="login-input-content">
+          <input type="text" required />
+          <span>Username</span>
+        </div>
+        <div className="login-input-content">
+          <input type="password" required />
+          <span>Password</span>
+        </div>
+        <button className="btn-submit" >Submit</button>
+
+        <div className="login-group">
+          <i className="fa-brands fa-google-plus-g" />
+          <i className="fa-brands fa-facebook-f"></i>
+
+        </div>
+      </div>
     </div>
   );
 }
